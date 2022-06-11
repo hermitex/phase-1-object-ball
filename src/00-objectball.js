ce60832acf5196d99eb0ea11676162eddb0c64e2
hermitex <mercytibs@yahoo.com>
2022-06-11 23:53:42 +0300

    Initial full implementation of all functions including bonus quizes

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -115,71 +115,203 @@ const gameObject = () => {
   };
 };
 
+const [homePlayers, awayPlayers] = [
+  gameObject().home.players,
+  gameObject().away.players,
+];
+
+const [homeTeam, awayTeam] = [gameObject().home, gameObject().away];
+
 //returns the points scored by the player
 //whose name has been provided
 const numPointsScored = (playerName) => {
-  const baseGameObject = gameObject();
-  let homePlayers = baseGameObject.home.players;
-  let awayPlayers = baseGameObject.away.players;
-
-  return homePlayers[playerName]
-    ? homePlayers[playerName].points
-    : awayPlayers[playerName].points;
+  if (Object.keys(homePlayers).includes(playerName)) {
+    return homePlayers[playerName].points;
+  } else if (Object.keys(awayPlayers).includes(playerName)) {
+    return awayPlayers[playerName].points;
+  } else {
+    return [];
+  }
 };
 
-console.log(numPointsScored('Bismak Biyombo'));
+// console.log(numPointsScored(""));
 
 //returns the shoe size of the player
 //whose name has been provided
 const shoeSize = (playerName) => {
-  return 0;
+  if (Object.keys(homePlayers).includes(playerName)) {
+    return homePlayers[playerName].shoe;
+  } else if (Object.keys(awayPlayers).includes(playerName)) {
+    return awayPlayers[playerName].shoe;
+  } else {
+    return [];
+  }
 };
+
+// console.log(shoeSize("Alanss"));
 
 //returns the colors of the team
 //whose name has been provided
 const teamColors = (teamName) => {
-  return "red and white";
+  if (homeTeam.teamName === teamName) {
+    return homeTeam.colors;
+  } else if (awayTeam.teamName === teamName) {
+    return awayTeam.colors;
+  } else {
+    return [];
+  }
 };
+
+// console.log(teamColors(""));
 
 //returns an array of teams
 const teamNames = (gameObject) => {
-  return [];
+  let teamNamesArray = [];
+  for (const key in gameObject) {
+    teamNamesArray.push(gameObject[key].teamName);
+  }
+  return teamNamesArray;
 };
 
+// console.log(teamNames(gameObject()));
+
 //returns an array of jersey numbers
-const playerNumbers = (teamName) => {
-  return [];
+const playerNumbers = (name) => {
+  const gameObj = gameObject();
+  let jerseyNumbersArray = [];
+  for (const key in gameObj) {
+    if (gameObj[key].teamName === name) {
+      for (const player in gameObj[key].players) {
+        jerseyNumbersArray.push(gameObj[key].players[player].number);
+      }
+    }
+  }
+  return jerseyNumbersArray;
 };
+
+// console.log(playerNumbers('Brooklyn Nets'));
 
 //returns the player statistics object of the player
 //whose name has been provided
 const playerStats = (playerName) => {
-  return {};
+  const gameObj = gameObject();
+  let playerStatsObj = {};
+  for (const key in gameObj) {
+    for (const player in gameObj[key].players) {
+      if (player === playerName) {
+        playerStatsObj = gameObj[key].players[player];
+      }
+    }
+  }
+  return playerStatsObj;
 };
+
+// console.log(playerStats(""));
 
 //returns the number of rebounds associated with
 //the player that has the largest shoe size
 const bigShoeRebounds = () => {
-  return "John Smith";
+  const gameObj = gameObject();
+  let bigShoeReoundPlayer = {};
+  let largestShoeSize = 0;
+  for (const key in gameObj) {
+    for (const player in gameObj[key].players) {
+      if (gameObj[key].players[player].shoe > largestShoeSize) {
+        largestShoeSize = gameObj[key].players[player].shoe;
+        bigShoeReoundPlayer = gameObj[key].players[player];
+      }
+    }
+  }
+
+  return bigShoeReoundPlayer.rebounds;
 };
+
+// console.log(bigShoeRebounds());
 
 //returns the player with most points
 const mostPointsScored = () => {
-  return "John Smith";
+  const gameObj = gameObject();
+  let playerWithHighestPoints = {};
+  let highestPoints = 0;
+  for (const key in gameObj) {
+    for (const player in gameObj[key].players) {
+      if (gameObj[key].players[player].points > highestPoints) {
+        highestPoints = gameObj[key].players[player].points;
+        playerWithHighestPoints = {};
+        playerWithHighestPoints[player] = gameObj[key].players[player];
+      }
+    }
+  }
+
+  return playerWithHighestPoints;
 };
+
+// console.log(mostPointsScored());
 
 //returns the team with most points
 const winningTeam = () => {
-  return "Charlotte Hornets";
+  const gameObj = gameObject();
+  let homeTeamPoints = 0;
+  let awayTeamPoints = 0;
+  for (const key in gameObj) {
+    if (key === "home") {
+      for (const player in gameObj[key].players) {
+        homeTeamPoints += gameObj[key].players[player].points;
+      }
+    } else if (key === "away") {
+      for (const player in gameObj[key].players) {
+        awayTeamPoints += gameObj[key].players[player].points;
+      }
+    }
+  }
+
+  return `
+  Winning Team: ${
+    homeTeamPoints > awayTeamPoints
+      ? gameObj["home"].teamName
+      : gameObj["away"].teamName
+  }
+  Points: ${homeTeamPoints > awayTeamPoints ? homeTeamPoints : awayTeamPoints}
+  `;
 };
+
+// console.log(winningTeam());
 
 //returns the player has the longest name
 const playerWithLongestName = () => {
-  return "John Smith";
+  const gameObj = gameObject();
+  let playerName = "";
+  let sizeOfLongestName = 0;
+  for (const key in gameObj) {
+    for (const player in gameObj[key].players) {
+      if (player.length > sizeOfLongestName) {
+        sizeOfLongestName = player.length;
+        playerName = player;
+      }
+    }
+  }
+
+  return playerName;
 };
+
+// console.log(playerWithLongestName());
 
 //returns true if the player with the
 //longest name had the most steals
 const doesLongNameStealATon = () => {
-  return false;
+  const gameObj = gameObject();
+  const nameOfPlayerWithLongestName = playerWithLongestName();
+  let mostSteals = 0;
+  let playerWithMostSteals = "";
+  for (const key in gameObj) {
+    for (const player in gameObj[key].players) {
+      if (gameObj[key].players[player].steals > mostSteals) {
+        mostSteals = gameObj[key].players[player].steals;
+        playerWithMostSteals = player;
+      }
+    }
+  }
+  return playerWithMostSteals === nameOfPlayerWithLongestName;
 };
+
+console.log(doesLongNameStealATon());
